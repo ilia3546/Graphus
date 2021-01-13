a02773eeb8f901d44eac9c7fd146c39461932133
Ilya Kharlamov <ilia3546@me.com>
2021-01-13 13:49:34 +0300

    Subclass QueryKeyedContainer on QueryContainer

--- Source/Codable/QueryKeyedContainer.swift
+++ Source/Codable/QueryKeyedContainer.swift
@@ -9,21 +9,7 @@
 import Foundation
 
 /// GraphQL query
-public class QueryKeyedContainer<Key: CodingKey> {
-    
-    internal var builder: QueryBuilder
-    
-    internal init(_ builder: QueryBuilder){
-        self.builder = builder
-    }
-    
-    public func addField(_ field: Field){
-        self.builder.fields.append(field)
-    }
-    
-    public func addFields(_ fields: [Field]){
-        self.builder.fields.append(contentsOf: fields)
-    }
+public class QueryKeyedContainer<Key: CodingKey>: QueryContainer {
     
     public func addField(_ keyedField: Key){
         self.addField(keyedField.stringValue)
@@ -40,12 +26,6 @@ public class QueryKeyedContainer<Key: CodingKey> {
     public func on(_ typeName: String, keyedClosure: @escaping (QueryKeyedContainer) -> Void) {
         let builder = QueryBuilder()
         keyedClosure(builder.query(keyedBy: Key.self))
-        self.builder.fields.append(OnQuery(typeName: typeName, fields: builder.fields))
-    }
-    
-    public func on(_ typeName: String, model: Queryable.Type, context: QueryBuilderContext? = nil) {
-        let builder = QueryBuilder()
-        model.buildQuery(with: builder, context: context)
         self.builder.fields.append(OnQuery(typeName: typeName, fields: builder.fields))
     }
     
